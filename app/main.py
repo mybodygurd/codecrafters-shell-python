b013f5b7934c74c84d2fa0b43aebe4f673557965
Mahdi <mybodygurd@gmail.com>
2025-05-06 17:10:40 +0330

    feature: Add type command to the shell

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 import sys
 
+builtins = {
+    "exit": "builtin",
+    "echo": "builtin",
+    "type": " builtin"
+}
+
 
 def main():
     while True:
@@ -12,6 +18,13 @@ def main():
             elif inp_line.startswith('echo '):
                 result = inp_line.split('echo ')[1]
                 print(result)
+            elif inp_line.startswith('type '):
+                result = inp_line.split('type ')[1].split()[0]
+                if result in builtins.keys():
+                    print(f"{result} is a shell {builtins[result]}")
+                else:
+                    print(f"{result}: not found")                
+
             else:    
                 print(f"{inp_line}: command not found")
                 
