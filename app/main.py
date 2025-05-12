0eac593118cdc6c6e4c07598e34e168f66121349
Mahdi <mybodygurd@gmail.com>
2025-05-13 00:03:57 +0330

    Clean the program code

--- app/main.py
+++ app/main.py
@@ -8,21 +8,14 @@ HOME_DIR = os.path.expanduser('~')
 PATH = os.environ['PATH']
 sep = os.pathsep
 
-builtins = {
-    "exit": "builtin",
-    "echo": "builtin",
-    "type": "builtin",
-    "pwd": "builtin"
-}
-
-def quote_delimiter_checker(string):
-    stack = []
-    for char in string:
-        if char == "'" and "'" not in stack:
-            stack.append("'")
-        elif char == "'" and "'" in stack:
-            stack.pop()
-    return not stack
+# def quote_delimiter_checker(string):
+#     stack = []
+#     for char in string:
+#         if char == "'" and "'" not in stack:
+#             stack.append("'")
+#         elif char == "'" and "'" in stack:
+#             stack.pop()
+#     return not stack
 
 def handle_executable_files(command):
     dirs = PATH.split(sep)
@@ -41,6 +34,76 @@ def handle_executable_files(command):
             except(NotADirectoryError, PermissionError, FileNotFoundError):
                 continue
     return None
+
+def handle_change_dir(tokens):
+    try:
+        if not tokens:
+            os.chdir(HOME_DIR)
+            
+        elif DIR_SEP in tokens:
+            dirs = tokens[0].split(DIR_SEP)
+            for dir in dirs:
+                if dir == '~':
+                    os.chdir(HOME_DIR)
+                    
+                elif dir == '..':
+                    current_dir = os.getcwd
+                    parent_dir = os.path.dirname(current_dir)
+                    os.chdir(parent_dir)
+                    
+                elif dir == '.':
+                    continue
+                else:
+                    os.chdir(dir)
+                    
+        elif tokens[0] == '~':
+            os.chdir(HOME_DIR)
+            
+        else:
+            path = tokens[0].strip()
+            os.chdir(path)
+            
+    except FileNotFoundError:
+        print(f"{path}: No such file or directory")
+        raise
+    except NotADirectoryError:
+        print(f"{path}: is not a directory")
+        raise
+    except PermissionError:
+        print(f"{path}: access denied")
+        raise
+    except OSError:
+        raise
+    
+def handle_exit(tokens: list):
+    exit_code = 0
+    try:
+        exit_code = int(tokens[0])
+    except ValueError:
+        sys.stderr.write("exit: numeric argument required\n")
+        exit_code = 1
+    sys.exit(exit_code)
+
+def handle_type(tokens: list):
+    for token in tokens:
+        if token in builtins:
+            print(f"{token} is a shell builtin")
+        else:    
+            path = handle_executable_files(token)
+            if path:
+                print(f"{token} is {path}")
+            else:
+                print(f"{token}: not found")
+ 
+
+builtins = {
+    "exit": handle_exit,
+    "echo": lambda x: print(' '.join(x)),
+    "type": "builtin",
+    "pwd": lambda x: print(os.getcwd()),
+    "cd": handle_change_dir
+}
+    
         
 def main():
     while True:
@@ -55,75 +118,11 @@ def main():
             parts = shlex.split(inp_line)
             command = parts[0]
             tokens = parts[1:]
-            n_tokens = len(tokens) - 1
             
-            if command == 'exit':
-                exit_code = 0
-                try:
-                    exit_code = int(tokens[0])
-                except ValueError:
-                    sys.stderr.write("exit: numeric argument required\n")
-                    exit_code = 1
-                sys.exit(exit_code)
+            if command in builtins:
+                function = builtins[command]
+                function(tokens)
                 
-            elif command == 'echo':
-                print(' '.join(tokens))
-                
-            elif command == 'pwd':
-                print(os.getcwd())
-                
-            elif command == 'cd':
-                try:
-                    if not tokens:
-                        os.chdir(HOME_DIR)
-                        
-                    elif DIR_SEP in tokens:
-                        dirs = tokens[0].split(DIR_SEP)
-                        for dir in dirs:
-                            if dir == '~':
-                                os.chdir(HOME_DIR)
-                                
-                            elif dir == '..':
-                                current_dir = os.getcwd
-                                parent_dir = os.path.dirname(current_dir)
-                                os.chdir(parent_dir)
-                                
-                            elif dir == '.':
-                                continue
-                            else:
-                                os.chdir(dir)
-                                
-                    elif tokens[0] == '~':
-                        os.chdir(HOME_DIR)
-                        
-                    else:
-                        path = tokens[0].strip()
-                        os.chdir(path)
-                        
-                except FileNotFoundError:
-                    print(f"{path}: No such file or directory")
-                    continue
-                except NotADirectoryError:
-                    print(f"{path}: is not a directory")
-                    continue
-                except PermissionError:
-                    print(f"{path}: access denied")
-                    continue
-                except OSError:
-                    continue
-                
-            elif command == 'type':
-                if not tokens:
-                    continue
-                for token in tokens:
-                    if token in builtins:
-                        print(f"{token} is a shell {builtins[token]}")
-                    else:    
-                        path = handle_executable_files(token)
-                        if path:
-                            print(f"{token} is {path}")
-                        else:
-                            print(f"{token}: not found")
             else:
                 path = handle_executable_files(command)
                 if path:
